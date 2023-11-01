1d112c08d05f1989c9a21753cce87b7ab5c5a6c8
Kasodariya Deep <deepkasodariya44@gmail.com>
2023-11-01 17:46:10 +0530

    Done with PLL ☠

--- src/practicals/lab4/LinkedPositionalList.java
+++ src/practicals/lab4/LinkedPositionalList.java
@@ -10,228 +10,268 @@ import java.util.*;
  * @author Kasodariya Deep
  * @version 1.0
  */
-public class LinkedPositionalList<E> implements PositionalList<E> {
-    private final Node<E> header;
-    private final Node<E> trailer;
-    private int size = 0;
+public class LinkedPositionalList<T> implements PositionalList<T> {
 
+    private int size;
+    private final Node<T> header;
+    private final Node<T> trailer;
+    /**
+     * Constructs an empty linked positional list.
+     */
     public LinkedPositionalList() {
-        header = new Node<>(null, null, null);
-        trailer = new Node<>(null, header, null);
-        header.setNext(trailer);
+        this.size = 0;
+        this.trailer = new Node<>(null, null, null);
+        this.header = new Node<>(null, null, this.trailer);
+        this.trailer.setPrev(this.header);
     }
 
     /**
      * Validates that the given position is a valid node in the list.
      *
-     * @param p - The position to be validated
-     * @return - The node corresponding to the given position
+     * @param p the position to be validated
+     * @return the node corresponding to the given position
      * @throws IllegalArgumentException if the position is not a valid node in the list
      */
-    private Node<E> validate(Position<E> p) throws IllegalArgumentException {
-        if (!(p instanceof Node<E> node)) throw new IllegalArgumentException("Invalid p");
-
+    private Node<T> validateNode(Position<T> p) throws IllegalArgumentException {
+        if (!(p instanceof Node))
+            throw new IllegalArgumentException("Passed position is invalid");
+        Node<T> node = (Node<T>) p;
         if (node.getNext() == null)
-            throw new IllegalArgumentException("p  is no longer in the list");
+            throw new IllegalArgumentException("The passed node is defunct");
         return node;
     }
 
     /**
      * Returns the position corresponding to the given node, or null if the node is the header or trailer.
      *
-     * @param node - The node to be converted to a position
-     * @return - The position corresponding to the given node, or null if the node is the header or trailer
+     * @param node the node to be converted to a position
+     * @return the position corresponding to the given node, or null if the node is the header or trailer
      */
-    private Position<E> position(Node<E> node) {
-        if (node == header || node == trailer) {
+    private Position<T> position(Node<T> node) {
+        if (node == this.header || node == this.trailer)
             return null;
-        }
-        return node;
+        else
+            return node;
+    }
+
+    /**
+     * Adds a new node with the given element between the given predecessor and successor nodes.
+     *
+     * @param t           the element to be stored in the new node
+     * @param predecessor the node that will be the predecessor of the new node
+     * @param successor   the node that will be the successor of the new node
+     * @return the position of the newly added node
+     */
+    private Position<T> addBetween(T t, Node<T> predecessor, Node<T> successor) {
+        Node<T> newNode = new Node<>(t, predecessor, successor);
+        predecessor.setNext(newNode);
+        successor.setPrev(newNode);
+        ++this.size;
+        return position(newNode);
     }
 
     /**
      * Returns the number of elements in the list.
      *
-     * @return - The number of elements in the list
+     * @return the number of elements in the list
      */
     public int size() {
-        return size;
+        return this.size;
     }
 
     /**
      * Returns true if the list is empty, false otherwise.
      *
-     * @return - true if the list is empty, false otherwise
+     * @return true if the list is empty, false otherwise
      */
     public boolean isEmpty() {
-        return size == 0;
+        return this.size == 0;
     }
 
     /**
      * Returns the position of the first element in the list, or null if the list is empty.
      *
-     * @return - The position of the first element in the list, or null if the list is empty
+     * @return the position of the first element in the list, or null if the list is empty
      */
-    public Position<E> first() {
-        return position(header.getNext());
+    public Position<T> first() {
+        return position(this.header.getNext());
     }
 
     /**
      * Returns the position of the last element in the list, or null if the list is empty.
      *
-     * @return - The position of the last element in the list, or null if the list is empty
+     * @return the position of the last element in the list, or null if the list is empty
      */
-    public Position<E> last() {
-        return position(trailer.getPrev());
+    public Position<T> last() {
+        return position(this.trailer.getPrev());
     }
 
     /**
      * Returns the position of the element immediately before the given position.
      *
-     * @param p - The position whose predecessor is to be returned
-     * @return - The position of the element immediately before the given position
+     * @param p the position whose predecessor is to be returned
+     * @return the position of the element immediately before the given position
      * @throws IllegalArgumentException if the given position is not a valid node in the list
      */
-    public Position<E> before(Position<E> p) throws IllegalArgumentException {
-        Node<E> node = validate(p);
+    public Position<T> before(Position<T> p) throws IllegalArgumentException {
+        Node<T> node = validateNode(p);
         return position(node.getPrev());
     }
 
     /**
      * Returns the position of the element immediately after the given position.
      *
-     * @param p - The position whose successor is to be returned
-     * @return - The position of the element immediately after the given position
+     * @param p the position whose successor is to be returned
+     * @return the position of the element immediately after the given position
      * @throws IllegalArgumentException if the given position is not a valid node in the list
      */
-    public Position<E> after(Position<E> p) throws IllegalArgumentException {
-        Node<E> node = validate(p);
+    public Position<T> after(Position<T> p) throws IllegalArgumentException {
+        Node<T> node = validateNode(p);
         return position(node.getNext());
-    }
-
-    /**
-     * Adds a new node with the given element between the given predecessor and successor nodes.
-     *
-     * @param e    - The element to be stored in the new node
-     * @param pred - The node that will be the predecessor of the new node
-     * @param succ - The node that will be the successor of the new node
-     * @return the position of the newly added node
-     */
-    private Position<E> addBetween(E e, Node<E> pred, Node<E> succ) {
-        Node<E> newest = new Node<>(e, pred, succ);
-        pred.setNext(newest);
-        succ.setPrev(newest);
-        size++;
-        return newest;
     }
 
     /**
      * Adds a new element to the beginning of the list.
      *
-     * @param e - The element to be added
-     * @return - The position of the newly added element
+     * @param t the element to be added
+     * @return the position of the newly added element
      */
-    public Position<E> addFirst(E e) {
-        return addBetween(e, header, header.getNext());
+    @Override
+    public Position<T> addFirst(T t) {
+        return addBetween(t, this.header, this.header.getNext());
     }
 
     /**
      * Adds a new element to the end of the list.
      *
-     * @param e - The element to be added
-     * @return - The position of the newly added element
+     * @param t the element to be added
+     * @return the position of the newly added element
      */
-    public Position<E> addLast(E e) {
-        return addBetween(e, trailer.getPrev(), trailer);
+    @Override
+    public Position<T> addLast(T t) {
+        return addBetween(t, this.trailer.getPrev(), this.trailer);
     }
 
-    public Position<E> addBefore(Position<E> p, E e) throws IllegalArgumentException {
-        Node<E> node = validate(p);
-        return addBetween(e, node.getPrev(), node);
+    /**
+     * Adds a new element immediately before the given position.
+     *
+     * @param p the position before which the new element is to be added
+     * @param t the element to be added
+     * @return the position of the newly added element
+     * @throws IllegalArgumentException if the given position is not a valid node in the list
+     */
+    @Override
+    public Position<T> addBefore(Position<T> p, T t) throws IllegalArgumentException {
+        Node<T> node = validateNode(p);
+        return addBetween(t, node.getPrev(), node);
     }
 
-    public Position<E> addAfter(Position<E> p, E e) throws IllegalArgumentException {
-        Node<E> node = validate(p);
-        return addBetween(e, node, node.getNext());
+    /**
+     * Adds a new element immediately after the given position.
+     *
+     * @param p the position after which the new element is to be added
+     * @param t the element to be added
+     * @return the position of the newly added element
+     * @throws IllegalArgumentException if the given position is not a valid node in the list
+     */
+    @Override
+    public Position<T> addAfter(Position<T> p, T t) throws IllegalArgumentException {
+        Node<T> node = validateNode(p);
+        return addBetween(t, node, node.getNext());
     }
 
-    public E set(Position<E> p, E e) throws IllegalArgumentException {
-        Node<E> node = validate(p);
-        E answer = node.getElement();
-        node.setElement(e);
-        return answer;
+    /**
+     * Replaces the element stored at the given position with the given element.
+     *
+     * @param p the position of the element to be replaced
+     * @param t the new element to be stored at the given position
+     * @return the old element that was replaced
+     * @throws IllegalArgumentException if the given position is not a valid node in the list
+     */
+    @Override
+    public T set(Position<T> p, T t) throws IllegalArgumentException {
+        Node<T> node = validateNode(p);
+        T temp = node.getElement();
+        node.setElement(t);
+        return temp;
     }
 
     /**
      * Removes the element stored at the given position from the list.
      *
-     * @param p - The position of the element to be removed
-     * @return - The element that was removed
+     * @param p the position of the element to be removed
+     * @return the element that was removed
      * @throws IllegalArgumentException if the given position is not a valid node in the list
      */
-    public E remove(Position<E> p) throws IllegalArgumentException {
-        Node<E> node = validate(p);
-        Node<E> pred = node.getPrev();
-        Node<E> succ = node.getNext();
-
-        pred.setNext(succ);
-        succ.setPrev(pred);
+    @Override
+    public T remove(Position<T> p) throws IllegalArgumentException {
+        Node<T> node = validateNode(p);
+        T temp = node.getElement();
+        node.getPrev().setNext(node.getNext());
+        node.getNext().setPrev(node.getPrev());
         size--;
-
-        E answer = node.getElement();
-        node.setElement(null);
+        // unlink node
         node.setPrev(null);
         node.setNext(null);
-
-        return answer;
-
-    }
-
-    public Iterator<Position<E>> iterator() {
-        return new PositionIterator();
+        node.setElement(null);
+        return temp;
     }
 
     /**
-     * A node in the doubly linked list.
+     * Returns an iterator of the elements stored in the list.
      *
-     * @param <E> the type of element held by this node
+     * @return an iterator of the elements stored in the list
      */
-    public static class Node<E> implements Position<E> {
-        private E element;
-        private Node<E> prev;
-        private Node<E> next;
+    public Iterable<Position<T>> positions() {
+        return new PositionIterable();
+    }
 
-        public Node(E e, Node<E> p, Node<E> n) {
+    /**
+     * Returns an iterator of the elements stored in the list.
+     *
+     * @return an iterator of the elements stored in the list
+     */
+    public Iterator<T> iterator() {
+        return new ElementIterator();
+    }
+
+    public static class Node<T> implements Position<T> {
+
+        private T element;
+        private Node<T> prev;
+        private Node<T> next;
+
+        public Node(T e, Node<T> p, Node<T> n) {
             this.element = e;
             this.prev = p;
             this.next = n;
         }
 
-        public E getElement() throws IllegalStateException {
-            if (next == null) {
-                throw new IllegalStateException("Position is no longer Valid");
-            }
-            return element;
+        public T getElement() throws IllegalStateException {
+            if (next == null)
+                throw new IllegalStateException("Position is no longer valid");
+            else
+                return element;
         }
 
-        public void setElement(E e) {
-            element = e;
+        public void setElement(T e) {
+            this.element = e;
         }
 
-        public Node<E> getPrev() {
+        public Node<T> getPrev() {
             return prev;
         }
 
-        public void setPrev(Node<E> p) {
-            prev = p;
+        public void setPrev(Node<T> p) {
+            this.prev = p;
         }
 
-        public Node<E> getNext() {
+        public Node<T> getNext() {
             return next;
         }
 
-        public void setNext(Node<E> n) {
-            next = n;
+        public void setNext(Node<T> n) {
+            this.next = n;
         }
     }
 
@@ -239,25 +279,105 @@ public class LinkedPositionalList<E> implements PositionalList<E> {
      * Returns an iterable representation of the list's positions.
      * Position Iterator is a private class that implements the Iterator interface and is used to iterate over the positions in the list.
      */
-    private class PositionIterator implements Iterator<Position<E>> {
-        private Position<E> cursor = first();
-        private Position<E> recent = null;
+    private class PositionIterator implements Iterator<Position<T>> {
 
+        Position<T> recent = null;
+        /**
+         * The cursor is the position of the next element to be returned.
+         * The recent is the position of the last element returned.
+         */
+        private Position<T> cursor = first();
+
+        /**
+         * Returns true if the iterator has a next element, false otherwise.
+         *
+         * @return true if the iterator has a next element, false otherwise
+         */
+        @Override
         public boolean hasNext() {
             return cursor != null;
         }
 
-        public Position<E> next() throws IllegalStateException {
-            if (cursor == null) throw new IllegalStateException("Nothing Left");
-            recent = cursor;
-            cursor = after(cursor);
-            return recent;
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if there are no more elements in the iteration
+         */
+        @Override
+        public Position<T> next() throws NoSuchElementException {
+            if (cursor == null)
+                throw new NoSuchElementException("No element found");
+            else {
+                recent = cursor;
+                cursor = after(cursor);
+                return recent;
+            }
         }
 
+        /**
+         * Removes the element returned by the most recent call to next().
+         *
+         * @throws IllegalStateException if there is no element to be removed
+         */
+        @Override
         public void remove() throws IllegalStateException {
-            if (recent == null) throw new IllegalStateException("Nothing to remove");
+            if (recent == null)
+                throw new IllegalStateException("No element can be removed");
             LinkedPositionalList.this.remove(recent);
             recent = null;
+        }
+    }
+
+    /**
+     * Position Iterable is a private class that implements the Iterable interface and is used to iterate over the positions in the list.
+     */
+
+    private class PositionIterable implements Iterable<Position<T>> {
+
+        public Iterator<Position<T>> iterator() {
+            return new PositionIterator();
+        }
+    }
+
+    /**
+     * Element Iterator is a private class that implements the Iterator interface and is used to iterate over the elements in the list.
+     */
+    private class ElementIterator implements Iterator<T> {
+        /**
+         * The position iterator used to iterate over the positions in the list.
+         */
+        Iterator<Position<T>> posIterator = new PositionIterator();
+
+        /**
+         * Returns true if the iterator has a next element, false otherwise.
+         *
+         * @return true if the iterator has a next element, false otherwise
+         */
+        @Override
+        public boolean hasNext() {
+            return posIterator.hasNext();
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if there are no more elements in the iteration
+         */
+        @Override
+        public T next() {
+            return posIterator.next().getElement();
+        }
+
+        /**
+         * Removes the element returned by the most recent call to next().
+         *
+         * @throws IllegalStateException if there is no element to be removed
+         */
+        @Override
+        public void remove() {
+            posIterator.remove();
         }
     }
 }
--- src/practicals/lab4/Main.java
+++ src/practicals/lab4/Main.java
@@ -1,21 +1,136 @@
 package practicals.lab4;
 
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Creating a list.
-        LinkedPositionalList<Integer> list = new LinkedPositionalList<>();
-        Position<Integer> p = list.addFirst(10);
-        p = list.addBefore(p, 20);
-        list.addFirst(30);
-        list.addBefore(p, 40);
+        Scanner sc = new Scanner(System.in);
+        LinkedPositionalList<String> list = new LinkedPositionalList<>();
+        int choice = 0;
 
-        // Iterating the list.
-        Iterator<Position<Integer>> itr = list.iterator();
-        while (itr.hasNext()) {
-            System.out.println(itr.next().getElement());
+        while(choice != 10) {
+            System.out.println("1. First element " + " 2. Last element " + "3. Add first "
+                    + " 4. Add last " + " 5. Add before " + " 6. Add after "
+                    + " 7. Remove " + " 8. Set " + " 9. Display " + " 10. Exit ");
+            System.out.println("Enter your choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1: {
+                    if (list.isEmpty()) System.out.println("Positional list is empty");
+                    else System.out.println("First element is: " + list.first().getElement());
+                    break;
+                }
+                case 2: {
+                    if (list.isEmpty()) System.out.println("Positional list is empty");
+                    else System.out.println("Last element is: " + list.last().getElement());
+                    break;
+                }
+                case 3: {
+                    System.out.println("Enter the element: ");
+                    String temp = sc.next();
+                    list.addFirst(temp);
+                    break;
+                }
+                case 4: {
+                    System.out.println("Enter the element: ");
+                    String temp = sc.next();
+                    list.addLast(temp);
+                    break;
+                }
+                case 5: {
+                    System.out.println("Enter the element: ");
+                    String element = sc.next();
+                    System.out.println("Enter the position: ");
+                    int position = sc.nextInt();
+
+                    if (position < 0 || position >= list.size()) {
+                        System.out.println("Invalid position");
+                    } else {
+                        Position<String> p = list.first();
+                        for (int i = 0; i < position; i++) {
+                            p = list.after(p);
+                        }
+                        list.addBefore(p, element);
+                    }
+                    break;
+                }
+                case 6: {
+                    System.out.println("Enter the element: ");
+                    String element = sc.next();
+                    System.out.println("Enter the position: ");
+                    int position = sc.nextInt();
+
+                    if (position < 0 || position >= list.size()) {
+                        System.out.println("Invalid position");
+                    } else {
+                        Position<String> p = list.first();
+                        for (int i = 0; i < position; i++) p = list.after(p);
+                        list.addAfter(p, element);
+                    }
+                    break;
+                }
+                case 7: {
+                    System.out.println("Enter the position: ");
+                    int pos = sc.nextInt();
+
+                    if (pos < 0 || pos >= list.size()) {
+                        System.out.println("Invalid position");
+                        break;
+                    }
+
+                    Position<String> p = list.first();
+                    for (int i = 0; i < pos; i++) {
+                        p = list.after(p);
+                    }
+                    list.remove(p);
+                    break;
+                }
+                case 8: {
+                    System.out.println("Enter the position: ");
+                    int pos = sc.nextInt();
+
+                    if (pos < 0 || pos >= list.size()) {
+                        System.out.println("Invalid position");
+                        break;
+                    }
+
+                    Position<String> p = list.first();
+                    for (int i = 0; i < pos; i++) {
+                        p = list.after(p);
+                    }
+
+                    System.out.println("Enter the element: ");
+                    String element = sc.next();
+                    list.set(p, element);
+                    break;
+                }
+                case 9:
+                    show(list);
+                    break;
+                default:
+                    System.out.println("Exiting the program ...");
+                    System.out.println("Thank you for using the program");
+                    break;
+            }
+        }
+
+        Iterable<Position<String>> positionIter = list.positions();
+        for (Position<String> p : positionIter) {
+            System.out.print(p.getElement() + " ");
+        }
+        System.out.println();
+    }
+
+    private static <T> void show(LinkedPositionalList<T> posList) {
+        if (posList.isEmpty()) {
+            System.out.println("Positional list is empty");
+        } else {
+            Iterator<T> i = posList.iterator();
+            while (i.hasNext()) System.out.print(" " + i.next());
+            System.out.println("\nSize of the positional list is: " + posList.size());
         }
     }
 }
